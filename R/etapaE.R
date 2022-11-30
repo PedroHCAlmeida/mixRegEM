23ceb1bc84515fe9288dd3dec6df3ab4f92bbdbb
Pedro Henrique Correa de Almeida <pedro.almeida@fibrasil.com.br>
2022-11-30 18:25:04 -0300

    Fix etapaE MixNormal

--- R/etapaE.R
+++ R/etapaE.R
@@ -7,7 +7,7 @@ etapaE.MixNormal = function(y, X, params, medias, args, ...){
   Z = sapply(1:args$g,
              function(j) params$P[j]*dnorm(y, medias[,j], params$params[j,"sigma"]))
 
-  Z = t(sapply(1:n, function(i) Z[i,]/sum(Z[i,])))
+  Z = t(sapply(1:args$n, function(i) Z[i,]/sum(Z[i,])))
 
   return(list(Z = Z))
 }
