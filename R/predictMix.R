b7606924c22e28433860c67cd933d9c52f29e879
Pedro Henrique Correa de Almeida <pedro.almeida@fibrasil.com.br>
2022-12-11 16:35:18 -0300

    fix predict

--- R/predictMix.R
+++ R/predictMix.R
@@ -1,10 +1,10 @@
 #' @param reg objeto de regressão de misturas
 #' @export
-predictMix = function(reg){
+predictMix = function(reg, x, r = NULL, class = T){
   UseMethod("predictMix")
 }
 #' @export
-predictMix.MoENormal = function(reg, x, r, type){
+predictMix.MoENormal = function(reg, x, r, class = T){
 
   args = list()
   args$n = nrow(x)
@@ -16,7 +16,7 @@ predictMix.MoENormal = function(reg, x, r, type){
 
   P = matrizP(alpha[,-ncol(alpha)], R)
   medias = estimaMedia.MoENormal(X, reg$params$params, args)
-  if(type == 1) y = apply(P*medias, 1, sum)
+  if(!class) y = apply(P*medias, 1, sum)
   else{
     grupos = apply(P, 1, which.max)
     y = sapply(1:args$n,
