534386fdb3c04a640a9f9760fc4b48414a3d1413
Pedro Henrique Correa de Almeida <pedro.almeida@fibrasil.com.br>
2023-04-04 15:10:14 -0300

    fix conv

--- R/regEM.R
+++ R/regEM.R
@@ -84,10 +84,10 @@ regEM = function(y, x, g = 2, ..., tol = 1E-6, family = "MixNormal",
 
   conv = T
   if((it == max_iter) && (it != min_iter)){
-    print("Warning: O algortimo parou pelo máximo de itereções, e não convergiu")
     if(verbose){
-      conv = F
+      print("Warning: O algortimo parou pelo máximo de itereções, e não convergiu")
     }
+    conv = F
     }
 
   veroAtual = vero(y, medias, paramsAtual, args)
